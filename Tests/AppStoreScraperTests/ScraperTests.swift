6c7e51f32b854aa41b5a810cafe19ac5e878e3cb
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2022-07-19 08:24:19 +0300

    Add tests

--- Tests/AppStoreScraperTests/ScraperTests.swift
+++ Tests/AppStoreScraperTests/ScraperTests.swift
@@ -2,4 +2,21 @@ import AppStoreScraper
 import XCTest
 
 final class ScraperTests: XCTestCase {
+    func testGetTopFreeApps() async throws {
+        let scraper = Scraper()
+        let ranking = try await scraper.getRanking(.topFree, country: .EE, limit: 5)
+        XCTAssertFalse(ranking.applications.isEmpty)
+    }
+
+    func testSearchApplications() async throws {
+        let scraper = Scraper()
+        let applications = try await scraper.searchApplications("pros and cons")
+        XCTAssertFalse(applications.isEmpty)
+    }
+
+    func testGetApplication() async throws {
+        let scraper = Scraper()
+        let application = try await scraper.getApplication(668357845)
+        XCTAssertNotNil(application)
+    }
 }
