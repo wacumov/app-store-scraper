a93cc4521b41e4222025ac684922850e8fd1135e
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2022-07-19 07:33:43 +0300

    Introduce RankingType enumeration

---
+++ Sources/AppStoreScraper/Models/RankingType.swift
@@ -0,0 +1,6 @@
+public enum RankingType: String {
+    case topFree, topFreeIpad, topFreeMac
+    case topPaid, topPaidIpad, topPaidMac
+    case topGrossing, topGrossingIpad
+    case new, newFree, newPaid
+}
