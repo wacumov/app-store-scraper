bb0d14db087a572f8b1cc170e3b689e7f4965abc
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2022-08-02 08:24:04 +0300

    Add Language

---
+++ Sources/AppStoreScraper/Models/Language.swift
@@ -0,0 +1,20 @@
+public enum Language: String, Codable, CaseIterable {
+    case ar_SA
+    case ca, cs
+    case da, de_DE
+    case el, en_AU, en_CA, en_GB, en_US, es_ES, es_MX
+    case fi, fr_CA, fr_FR
+    case he, hi, hr, hu
+    case id, it
+    case ja
+    case ko
+    case ms
+    case nl_NL, no
+    case pl, pt_BR, pt_PT
+    case ro, ru
+    case sk, sv
+    case th, tr
+    case uk
+    case vi
+    case zh_Hans, zh_Hant
+}
